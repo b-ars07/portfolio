2d1ac0dc4c1dacdcfc19427af02bded33f9a83e1
Arseniy Berezin <b-ars07@yandex.ru>
2017-06-08 23:32:11 +0300

    initialize project

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var 
+	gulp = require('gulp'),
+	browserSync = require('browser-sync');
+
+	gulp.task('server', function () {
+		browserSync({
+			port: 8080,
+			server: {
+				baseDir: 'app'
+			}
+		});
+	});
+
+	gulp.task('watch', function () {
+		gulp.watch([
+			'app/*.html',
+			'app/js/**/*.js',
+			'app/css/**/.css'
+			]).on('change', browserSync.reload);
+	});
+
+	gulp.task('default', ['server', 'watch']);
